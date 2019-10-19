72d1dd3f9b8e927186cbd8892a11c7ec88260363
AymericArn <aymericarnoult@gmail.com>
2019-10-19 15:21:10 +0200

    :sparkles: :tada: voice recognition ok !

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
+import Vuex, { Store } from 'vuex';
 import State from '@/models/IStore';
 // import type {MediaRecorder} from 'dom-mediacapture-record';
+import axios from 'axios';
+import Recorder from 'recorder-js';
 
+// declare var Recorder: any;
 declare var MediaRecorder: any;
-
 Vue.use(Vuex);
 
 // const createMediaRecorder = async (): Promise<any> => {
@@ -15,49 +17,53 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     // // mocking the MediaRecorder to please TS
+    stream: null,
     mediaRecorder: null as any,
+    audioContext: new (window.AudioContext)(),
     audioChunks: [] as any[],
+    audioBlob: null as Blob,
+    audioWav: null,
     user: {
       name: 'Aymeric',
     },
     isRecording: false,
+    wav: null,
   },
   mutations: {
     setMediaRecorder(state, payload) {
+      state.stream = payload.stream;
       state.mediaRecorder = payload.mediaRecorder;
     },
     // start the recording of the voice
     startRecord(state) {
       if (state.mediaRecorder !== null) {
-        state.mediaRecorder.start();
-
-        state.mediaRecorder.addEventListener('dataavailable', (event: any) => {
-          state.audioChunks.push(event.data);
-        });
-
-        state.isRecording = true;
+        state.mediaRecorder.start().then(() => state.isRecording = true);
       }
     },
     stopRecord(state) {
-      state.mediaRecorder.stop();
-      state.mediaRecorder.addEventListener('stop', () => {
-        const audioBlob = new Blob(state.audioChunks);
-        const audioUrl = URL.createObjectURL(audioBlob);
-        const audio = new Audio(audioUrl);
-        console.log(audio);
+      state.mediaRecorder.stop().then(({blob, buffer}) => {
+        state.audioBlob = blob;
       });
-      state.isRecording = false;
     },
   },
   actions: {
     loadMediaRecorder(context) {
-      console.log('suce');
       return new Promise((resolve, reject) => {
-        navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
-          const mediaRecorder = new MediaRecorder(stream);
-          context.commit('setMediaRecorder', {mediaRecorder});
-          resolve();
-        }).catch((err) => console.log(err));
+        navigator.mediaDevices
+          .getUserMedia({ audio: true })
+          .then((stream) => {
+
+            const recorder = new Recorder(context.state.audioContext, {
+              onAnalyze: (data) => console.log(data)
+            });
+            recorder.init(stream);
+
+            context.commit('setMediaRecorder', { mediaRecorder: recorder, stream });
+            resolve();
+          })
+          .catch((err) => {
+            alert('No mic detected');
+          });
       });
     },
     startRecording(context) {
@@ -65,6 +71,25 @@ export default new Vuex.Store({
     },
     stopRecording(context) {
       context.commit('stopRecord');
+      setTimeout(() => {
+        context.dispatch('sendAudioToAPI');
+      }, 300);
+    },
+    async sendAudioToAPI(context) {
+      axios({
+        method: 'post',
+        url: 'http://localhost:3000/audio',
+        data: context.state.audioBlob,
+        headers: {
+          'Content-Type':'audio/wav',
+        },
+      }).then((response) => {
+        //handle success
+        console.log(response);
+      }).catch((response) => {
+        //handle error
+        console.log(response);
+      });
     },
   },
 });
